a43c02d785b2ec02ee850c1ea44009b6890b63fb
Sofia Fernandez <s.oofi@hotmail.com>
2021-12-21 16:10:11 -0300

    Navbar

---
+++ src/component/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <h3 className="Titulo">PULCUWINES</h3>
+            <nav>
+                <li>
+                    <a href="/#">Inicio</a>
+                </li>
+                <li>
+                    <a href="/#">Productos</a>
+                </li>
+                <li>
+                    <a href="/#">Contacto</a>
+                </li>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
